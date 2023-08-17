e50c0c18b65e29c43d1fa58489ec7835fc1c8ac9
Max-vS <max@vonstorch.com>
2023-08-17 12:53:08 +0200

    hero

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,10 @@
 import './globals.css'
 import type {Metadata} from 'next'
-import {Inter} from 'next/font/google'
 import React from "react";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import {Analytics} from "@vercel/analytics/react";
-
-
-const inter = Inter({ subsets: ['latin'] })
+import {inter} from "./fonts"
 
 export const metadata: Metadata = {
   title: 'Create Next App',
