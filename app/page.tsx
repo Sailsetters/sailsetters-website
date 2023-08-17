5c0b445a086051b0974cca1f1276820172548251
navidrajaei <navid.rajaei@tum.de>
2023-08-17 13:30:26 +0200

    moved Hero back

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,27 @@
 import Head from 'next/head'
-import Hero from '@/components/Hero';
+import Image from 'next/image'
 
 import React from 'react';
+
+function Hero() {
+    return (
+        <section className="relative h-screen w-screen flex">
+            <Image
+                src="/brooke-cagle-g1Kr4Ozfoac-unsplash.jpg"
+                alt="Background Image"
+                layout="fill"
+                objectFit="cover"
+                quality={100}
+                className="absolute z-0"
+            />
+
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10">
+                <h1 className="text-6xl font-semibold text-white uppercase tracking-widest">Sailsetters</h1>
+                <h2 className="text-4xl text-white mt-2">Students for Change.</h2>
+            </div>
+        </section>
+    );
+}
 
 export default function Home() {
   return (
