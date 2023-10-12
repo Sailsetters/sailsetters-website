957bdba6bee69fd3f4f088a31e5e9dc7ee18d32b
navidrajaei <navid.rajaei@tum.de>
2023-10-12 18:07:49 +0200

    added timeline

--- app/page.tsx
+++ app/page.tsx
@@ -88,8 +88,13 @@ interface ProjectCardProps {
   }
 
 function ProjectCard({ title, description }: ProjectCardProps) {
+    const top = Math.random() * 80 + 10 + '%';
+    const left = Math.random() * 70 + 10 + '%';
+
     return (
         <div className="group relative p-6 rounded-lg backdrop-blur-md shadow-lg hover:shadow-2xl transition-all duration-300">
+            {/* Blue orb */}
+        <div className="absolute w-16 h-16 bg-blue-500/70 blur-xl rounded-full opacity-40" style={{ top, left }}></div>
         <h4 className="text-xl font-semibold mb-4">{title}</h4>
         <p className="text-gray-700">{description}</p>
     </div>
@@ -102,19 +107,19 @@ function Projects() {
             <h2 id="projekte" className="text-5xl text-gray-800 text-center uppercase tracking-wide mb-16 font-medium">Projekte</h2>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
                 <ProjectCard
-                    title="Mentoring @ StartStark"
+                    title="Mentoring"
                     description="Bei unserem Mentoring Projekt, „matched“ unser Haven StartStark dich mit einem Mentee, einem Kind oder Jugendlichen, das regelmäßig das Gate6 besucht. Die Idee hinter dem Mentoring, ist den Mentees als eine 1:1 Bezugsperson zur Seite zu stehen. Dabei kann und wird die Mentor-Mentee Beziehung natürlich bei jedem Paar anders aussehen."
                 />
                 <ProjectCard
-                    title="Spieleabend @ Lichtblick Hasenbergl"
+                    title="Spieleabend"
                     description="Der Spieleabend ist ein Projekt mit unserem Haven Lichtblick Hasenbergl, bei dem du, zusammen mit mindestens einem anderen Sailsetter, einmal im Monat den Lichtblick Hasenbergl besuchst und einen Spieleabend veranstaltest. Dabei lernt ihr zuvor die Spielregeln und spielt dieses dann zusammen mit einer kleinen Gruppe an Kindern."
                 />
                 <ProjectCard
-                    title="Projektwochen Naturwissenschaften @ Lichtblick Hasenbergl"
+                    title="Projektwochen Naturwissenschaften"
                     description="Die Projektwochen Naturwissenschaften sind ein Projekt am Kindergarten unseres Havens Lichtblick Hasenbergl. Dabei bereitest du zusammen mit ein bis drei andern Sailsettern naturwissenschaftliche Experimente für Kindergartenkinder vor. Diese finden dann in vier aufeinanderfolgenden Wochen im Januar 2024 jeweils an einem Vormittag pro Woche statt."
                 />
                 <ProjectCard
-                    title="Verstehendes Lesen @ Lichtblick Hasenbergl"
+                    title="Verstehendes Lesen"
                     description="Verstehendes Lesen, ist ein 1:1 Projekt mit unserem Haven Lichtblick Hasenbergl. Einmal pro Woche liest du gemeinsam mit einem Kind im Grundschulalter. Dabei geht es darum das Textverständnis des Kindes zu verbessern. Für dieses Projekt suchen wir explizit nach Pädagogik- und Lehramt-Studierenden."
                 />
                 <ProjectCard
@@ -137,13 +142,88 @@ function Partners() {
         <Section>
             <h2 id="partner" className="text-5xl text-gray-800 text-center uppercase tracking-wide mb-16 font-medium">Unsere Partner</h2>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 place-items-center">
-                {partners.map(partner => (
+                {partners.map(
+                    partner => (
                     <div className="flex items-center justify-center w-[250px] h-[250px]">
                         <a href={partner.link} target="_blank" rel="noopener noreferrer" key={partner.name} className="w-full transition-transform transform hover:scale-105">
                             <img src={partner.logo} alt={partner.name} className="max-w-full max-h-full flex items-center justify-center" />
                         </a>
                     </div>
                 ))}
+            </div>
+        </Section>
+    );
+}
+
+interface TimelineEventProps {
+    start: string;
+    end: string;
+    title: string;
+    description: string;
+  }
+
+function TimelineEvent({ start, end, title, description}: TimelineEventProps ) {
+    return (
+        <div className="grid grid-cols-2">
+            <div className='mr-0 mb-6'>
+                <h4 className="text-right text-xl font-semibold">{start}</h4>
+                <h4 className="text-right pb-8 text-xl font-semibold"> {end}</h4>
+            </div>
+            <div className="pb-4 ml-4 pl-2 border-l-4 border-black">
+                <h5 className=" text-lg font-semibold">{title}</h5>
+                <p className="text-gray-700">{description}</p>
+            </div>
+        </div>
+    );
+}
+
+function Timeline() {
+    return (
+        <Section>
+            <h2 className="text-5xl text-gray-800 text-center uppercase tracking-wide mb-16 font-medium">Timeline</h2>
+            <div className="relative max-w-3xl mx-auto">
+                <div className="grid grid-cols-2">
+                    <div className='mr-0 mb-6'>
+                    </div>
+                    <div className=" ml-4 pl-2 border-l-4 border-black border-dashed">
+                    </div>
+                </div>
+                <TimelineEvent
+                    start="16.10.23 -"
+                    end="29.10.23"
+                    title="Berwerbungsphase"
+                    description=""
+                />
+                <TimelineEvent
+                    start="30.10.23 -"
+                    end="03.11.23"
+                    title="Interviewphase"
+                    description=""
+                />
+                <TimelineEvent
+                    start="04.11.23"
+                    end=""
+                    title="Onboarding Day"
+                    description="Onboarding Day für alle neuen Sailsetter."
+                />
+                <TimelineEvent
+                    start="05.11.23 -"
+                    end="09.02.24"
+                    title="Projektphase"
+                    description="In dieser Phase arbeiten alle unsere Sailsetter an verschiedenen Projekten mit."
+                />
+                <TimelineEvent
+                    start="10.02.24"
+                    end=""
+                    title="Reflection Day"
+                    description="An diesem Tag treffen sich alle aktiven Sailsetter und tauschen in einem Workshop Format ihre Erfahrungen, die sie auf ihren Projekten gemacht haben, aus. Danach gemeinsames Socializing und feiern."
+                />
+                <div className="grid grid-cols-2">
+                    <div className='mr-0 mb-6'>
+                    </div>
+                    <div className=" ml-4 pl-2 border-l-4 border-black border-dashed">
+                    </div>
+                </div>
             </div>
         </Section>
     );
@@ -161,6 +241,7 @@ export default function Home() {
     <Statements />
     <Projects />
     <Partners />
+    <Timeline />
    </>
   )
 }
