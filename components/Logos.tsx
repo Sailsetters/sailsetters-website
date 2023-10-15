32fbe3d89e4ab040775765ea38b8de11eae7bbb1
Max-vS <max@vonstorch.com>
2023-10-15 13:13:23 +0200

    logos hover

--- components/Logos.tsx
+++ components/Logos.tsx
@@ -15,7 +15,7 @@ export default function Logos({ logos }: Props) {
             <div className="flex flex-wrap items-center justify-center space-y-4">
                 {logos.map((logo) => (
                     <div
-                        className="h-32 w-full px-4 py-2 grayscale transition-all duration-200 ease-out hover:grayscale-0 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5"
+                        className="h-32 w-full px-4 py-2 grayscale transition-all duration-200 ease-out hover:grayscale-0 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 hover:scale-105 transform-gpu"
                         key={logo.src}
                     >
                         <Link href={logo.href} passHref>
@@ -23,8 +23,8 @@ export default function Logos({ logos }: Props) {
                                 <Image
                                     src={logo.src}
                                     alt={logo.alt || ""}
-                                    width={160}
-                                    height={50}
+                                    width={200}
+                                    height={100}
                                     className="object-contain"
                                 />
                             </div>
