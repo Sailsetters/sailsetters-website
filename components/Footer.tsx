b9efabf8eb0d60190d38f83a86336ca57ccfe9e3
Max-vS <max@vonstorch.com>
2023-10-16 12:03:59 +0200

    change footer background color

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,7 +9,7 @@ const Footer = () => {
 
   return (
     <footer>
-      <div className="bg-blue-900">
+      <div className="bg-blue-800">
         <div className="max-w-6xl m-auto text-gray-800 flex flex-wrap justify-left">
           {/* Col-1 */}
           <div className="p-5 w-1/2 sm:w-4/12 md:w-3/12">
