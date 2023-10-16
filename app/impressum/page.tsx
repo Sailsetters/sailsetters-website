a71a490ef70d657109e51077441e1f992807ad32
navidrajaei <navid.rajaei@tum.de>
2023-10-17 00:00:55 +0200

    added telefax number

--- app/impressum/page.tsx
+++ app/impressum/page.tsx
@@ -33,7 +33,7 @@ function Impressum() {
                         <h1 className='text-xl font-bold mb-3 mt-5 text-black' >Kontakt</h1>
                         <p className="mb-3 text-black">
                             <span className="font-semibold">Telefon:</span> +49 176 32369363<br/>
-                            {/* <span className="font-semibold">Telefax:</span> +49 (0) 123 44 55 99<br/> */}
+                            <span className="font-semibold">Telefax:</span> +49 89 54645686<br/>
                             <span className="font-semibold">E-Mail:</span> kontakt@sailsetters.de
                         </p>
                         <h1 className='text-xl font-bold mb-3 mt-5 text-black' >Redaktionell verantwortlich</h1>
