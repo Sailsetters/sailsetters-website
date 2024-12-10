e4d80287be719526ab0d42cd2f716a87abc60fa9
navidrajaei <navid.rajaei@tum.de>
2024-12-10 19:44:29 +0100

    Added satzung as pdf

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import Link from 'next/link'
-import {faInstagram, faFacebook, faLinkedin} from '@fortawesome/free-brands-svg-icons'
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-
+import React from "react";
+import Link from "next/link";
+import {
+  faInstagram,
+  faFacebook,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Footer = () => {
-  const currentYear = new Date().getFullYear()
+  const currentYear = new Date().getFullYear();
 
   return (
     <footer>
@@ -18,13 +21,22 @@ const Footer = () => {
               Sailsetters
             </div>
             {/* Links */}
-            <Link href="/#statements" className="my-3 block text-linen hover:pl-[8px] text-sm font-medium duration-300">
+            <Link
+              href="/#statements"
+              className="my-3 block text-linen hover:pl-[8px] text-sm font-medium duration-300"
+            >
               Über uns
             </Link>
-            <Link href="/#projects" className="my-3 block text-linen hover:pl-[8px] text-sm font-medium duration-300">
+            <Link
+              href="/#projects"
+              className="my-3 block text-linen hover:pl-[8px] text-sm font-medium duration-300"
+            >
               Projekte
             </Link>
-            <Link href="/impressum" className="my-3 block text-linen hover:pl-[8px] text-sm font-medium duration-300">
+            <Link
+              href="/impressum"
+              className="my-3 block text-linen hover:pl-[8px] text-sm font-medium duration-300"
+            >
               Kontakt
             </Link>
           </div>
@@ -35,29 +47,49 @@ const Footer = () => {
               Rechtliches
             </div>
             {/* Links */}
-            <Link href="/impressum" className="my-3 block text-linen hover:pl-[8px] text-sm font-medium duration-300">
+            <Link
+              href="/impressum"
+              className="my-3 block text-linen hover:pl-[8px] text-sm font-medium duration-300"
+            >
               Impressum
             </Link>
-            
+            <Link
+              href="/satzung.pdf"
+              className="my-3 block text-linen hover:pl-[8px] text-sm font-medium duration-300"
+            >
+              Satzung
+            </Link>
           </div>
           {/* Col-3 */}
           <div className="p-5 w-1/2 sm:w-4/12 md:w-3/12">
             <div className="text-xs uppercase text-blood font-medium mb-6">
               Social
             </div>
-            <ul className='flex gap-5' >
-               <li>
-                <Link href="https://www.instagram.com/sailsetters/" target="_blank" className="my-3 block text-linen hover:text-[#cd486b] text-sm font-medium duration-300">
+            <ul className="flex gap-5">
+              <li>
+                <Link
+                  href="https://www.instagram.com/sailsetters/"
+                  target="_blank"
+                  className="my-3 block text-linen hover:text-[#cd486b] text-sm font-medium duration-300"
+                >
                   <FontAwesomeIcon width={20} height={20} icon={faInstagram} />
                 </Link>
               </li>
               <li>
-                <Link href="https://www.facebook.com/sailsetters" target="_blank" className="my-3 block text-linen hover:text-[#4267B2] text-sm font-medium duration-300">
+                <Link
+                  href="https://www.facebook.com/sailsetters"
+                  target="_blank"
+                  className="my-3 block text-linen hover:text-[#4267B2] text-sm font-medium duration-300"
+                >
                   <FontAwesomeIcon width={20} height={20} icon={faFacebook} />
                 </Link>
               </li>
               <li>
-                <Link href="https://www.linkedin.com/company/sailsetters" target="_blank" className="my-3 block text-linen hover:text-[#0e76a8] text-sm font-medium duration-300">
+                <Link
+                  href="https://www.linkedin.com/company/sailsetters"
+                  target="_blank"
+                  className="my-3 block text-linen hover:text-[#0e76a8] text-sm font-medium duration-300"
+                >
                   <FontAwesomeIcon width={20} height={20} icon={faLinkedin} />
                 </Link>
               </li>
@@ -82,11 +114,24 @@ const Footer = () => {
               </form>
             </div>
           </div>*/}
-        </div> 
+        </div>
         {/* Add a samll copyright message */}
         <div className="items-center flex pb-5 px-3 m-auto pt-5 border-t border-blood text-blood text-sm flex-col max-w-6xl">
           <p>
-            Designed by <Link className="text-linen hover:text-blue-600" href="https://www.linkedin.com/in/maxvonstorch/">Max von Storch</Link> and <Link className="text-linen hover:text-blue-600" href="https://www.linkedin.com/in/navid-rajaei-a405ab251/">Navid Rajaei</Link>
+            Designed by{" "}
+            <Link
+              className="text-linen hover:text-blue-600"
+              href="https://www.linkedin.com/in/maxvonstorch/"
+            >
+              Max von Storch
+            </Link>{" "}
+            and{" "}
+            <Link
+              className="text-linen hover:text-blue-600"
+              href="https://www.linkedin.com/in/navid-rajaei-a405ab251/"
+            >
+              Navid Rajaei
+            </Link>
           </p>
           <p className="mt-2">
             © {currentYear} Sailsetters e.V. Alle Rechte vorbehalten.
@@ -94,7 +139,7 @@ const Footer = () => {
         </div>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
